6dbe342fea15fe8333b882e18fb10f89d6c70687
Maksym Nemera <nickmitchell@ukr.net>
2024-05-01 19:49:28 +0300

    Updates

---
+++ index.c06c11a4.js
@@ -0,0 +1,6 @@
+// nav
+$(document).ready(function(){$(".sidenav").sidenav()}),// material box
+$(document).ready(function(){$(".materialboxed").materialbox()}),// slider
+$(document).ready(function(){$(".carousel").carousel()});//# sourceMappingURL=index.c06c11a4.js.map
+
+//# sourceMappingURL=index.c06c11a4.js.map
