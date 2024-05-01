3ddc6b7df99d645df2ac019e0e4a85a54e7cdcd7
Maksym Nemera <nickmitchell@ukr.net>
2024-05-01 19:48:44 +0300

    add init code and remove unrequire code

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+// nav
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  // material box
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+  // slider
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
